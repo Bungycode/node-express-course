2161c79e4f727c7a1f52cc9c55573bb39fb59efe
Andrew Bungcayao <thedevandrew@gmail.com>
2023-07-06 13:16:50 -0400

    More refactoring

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -42,57 +42,6 @@ app.use("/api/v1/products", productsRoutes);
 app.use("/api/v1/query", queryRoutes);
 
 // Catch all end point for end points that don't exist
-app.get("/api/v1/test", (req, res) => {
-  res.json({ message: "It worked!" });
-});
-
-app.get("/api/v1/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/v1/products/:productID", (req, res) => {
-  const idToFind = parseInt(req.params.productID); // because this will be a string, and we need an integer
-
-  const validProduct = products.some((p) => {
-    return p.id === idToFind;
-  });
-
-  if (validProduct) {
-    // p.id = idToFind causes an unwanted mutation to the data. It causes the first product to not exist and the rest of the products to be the albany sofa.
-    const product = products.find((p) => p.id === idToFind);
-    res.status(200).json(product);
-  } else if (!validProduct) {
-    res.status(404).json({ message: "That product was not found." });
-  }
-});
-
-app.get("/api/v1/query", (req, res) => {
-  const { search, limit, price } = req.query;
-
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = products.filter((product) =>
-      product.name.startsWith(search)
-    );
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, parseInt(limit));
-  }
-
-  if (price) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.price <= price;
-    });
-  }
-
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-  return res.status(200).json(sortedProducts);
-});
-
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Page not found - 404</h1>");
 });
